4f7c5c9546b2f485609176516705b067d7fbc139
Sabbir Ahmed <sabbir.081070@gmail.com>
2018-12-12 01:59:42 +0600

    chore: initial setup and removing unwanted codes

--- woo-quick-cart-checkout.php
+++ woo-quick-cart-checkout.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WooCommerce Quick Cart and Checkout
 Plugin URI: http://sabbir.pro/
-Description: Instanct cart and checkout for customers
+Description: Instant cart and checkout for customers
 Version: 1.0.0
 Author: Sabbir Ahmed
 Author URI: http://sabbir.pro/
@@ -10,7 +10,7 @@ License: GPL2
 */
 
 /**
- * Copyright (c) YEAR Sabbir Ahmed (email: sabbir@wedevs.com). All rights reserved.
+ * Copyright (c) YEAR Sabbir Ahmed (email: sabbir.081070@gmail.com). All rights reserved.
  *
  * Released under the GPL license
  * http://www.opensource.org/licenses/gpl-license.php
@@ -43,7 +43,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
  *
  * @class Woo_Quick_Cart_Checkout The class that holds the entire Woo_Quick_Cart_Checkout plugin
  */
-class Woo_Quick_Cart_Checkout {
+final Woo_Quick_Cart_Checkout {
 
      /**
      * Plugin version
@@ -51,6 +51,13 @@ class Woo_Quick_Cart_Checkout {
      * @var string
      */
     public $version = '1.0.0';
+
+    /**
+     * Minimum PHP version required
+     *
+     * @var string
+     */
+    private $min_php = '5.6.0';
 
     /**
      * Constructor for the Woo_Quick_Cart_Checkout class
@@ -68,17 +75,11 @@ class Woo_Quick_Cart_Checkout {
         // Define all constant
         $this->define_constant();
 
-        //includes file
-        $this->includes();
+        if ( ! $this->is_supported_php() ) {
+            return;
+        }
 
-        // init actions and filter
-        $this->init_filters();
-        $this->init_actions();
-
-        // initialize classes
-        $this->init_classes();
-
-        do_action( 'woo_quick_cart_checkout_loaded', $this );
+        add_action( 'woocommerce_loaded', [ $this, 'init_plugin' ] );
     }
 
     /**
@@ -98,24 +99,6 @@ class Woo_Quick_Cart_Checkout {
     }
 
     /**
-     * Placeholder for activation function
-     *
-     * Nothing being called here yet.
-     */
-    public static function activate() {
-
-    }
-
-    /**
-     * Placeholder for deactivation function
-     *
-     * Nothing being called here yet.
-     */
-    public static function deactivate() {
-
-    }
-
-    /**
     * Defined constant
     *
     * @since 1.0.0
@@ -129,6 +112,33 @@ class Woo_Quick_Cart_Checkout {
         define( 'WOO_QUICK_CART_CHECKOUT_ASSETS', plugins_url( '/assets', WOO_QUICK_CART_CHECKOUT_FILE ) );
     }
 
+    /**
+     * Check if the PHP version is supported
+     *
+     * @return bool
+     */
+    public function is_supported_php() {
+        if ( version_compare( PHP_VERSION, $this->min_php, '<=' ) ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Init plugin files after loaded WooCommerce
+     *
+     * @since 1.0.0
+     *
+     * @return void
+     */
+    public function init_plugin() {
+        $this->includes();
+
+        $this->init_hooks();
+
+        do_action( 'woo_quick_cart_checkout_loaded', $this );
+    }
     /**
     * Includes all files
     *
@@ -147,8 +157,10 @@ class Woo_Quick_Cart_Checkout {
     *
     * @return void
     **/
-    private function init_filters() {
-        // Load all filters
+    private function init_hooks() {
+        // Localize our plugin
+        add_action( 'init', array( $this, 'localization_setup' ) );
+        add_action( 'init', array( $this, 'init_classes' ) );
     }
 
     /**
@@ -158,25 +170,8 @@ class Woo_Quick_Cart_Checkout {
     *
     * @return void
     **/
-    private function init_actions() {
-        // Localize our plugin
-        add_action( 'init', array( $this, 'localization_setup' ) );
-
-        // Loads frontend scripts and styles
-        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-        add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
-        add_action( 'wp_footer', array( $this, 'load_template' ), 10 );
-    }
-
-    /**
-    * Inistantiate all classes
-    *
-    * @since 1.0.0
-    *
-    * @return void
-    **/
     private function init_classes() {
-        // Create instnace for all class
+
     }
 
     /**
@@ -190,86 +185,6 @@ class Woo_Quick_Cart_Checkout {
         load_plugin_textdomain( 'woo-quick-cart-checkout', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
     }
 
-    /**
-     * Enqueue admin scripts
-     *
-     * @since 1.0.0
-     *
-     * Allows plugin assets to be loaded.
-     *
-     * @return void
-     */
-    public function enqueue_scripts() {
-
-        wp_enqueue_style( 'wooqcc-styles', WOO_QUICK_CART_CHECKOUT_ASSETS . '/css/style.css', false, date( 'Ymd' ) );
-        wp_enqueue_script( 'wooqcc-scripts', WOO_QUICK_CART_CHECKOUT_ASSETS . '/js/script.js', array( 'jquery' ), false, true );
-
-        /**
-         * Example for setting up text strings from Javascript files for localization
-         *
-         * Uncomment line below and replace with proper localization variables.
-         */
-        // $translation_array = array( 'some_string' => __( 'Some string to translate', 'baseplugin' ), 'a_value' => '10' );
-        // wp_localize_script( 'base-plugin-scripts', 'baseplugin', $translation_array ) );
-    }
-
-    /**
-     * Load template
-     *
-     * @since 1.0.0
-     *
-     * @return void
-     */
-    public function load_template() {
-        ?>
-        <div class="cd-cart-container">
-            <a href="#0" class="cd-cart-trigger">
-                Cart
-                <ul class="count"> <!-- cart items count -->
-                    <li>0</li>
-                    <li>0</li>
-                </ul> <!-- .count -->
-            </a>
-
-            <div class="cd-cart">
-                <div class="wrapper">
-                    <header>
-                        <h2>Cart</h2>
-                        <span class="undo">Item removed. <a href="#0">Undo</a></span>
-                    </header>
-
-                    <div class="body">
-
-                        <ul>
-                            <?php foreach ( WC()->cart->get_cart_contents() as $key => $cart_item ): ?>
-
-                            <?php endforeach ?>
-                        </ul>
-                    </div>
-
-                    <footer>
-                        <a href="#0" class="checkout btn"><em>Checkout - $<span>0</span></em></a>
-                    </footer>
-                </div>
-            </div> <!-- .cd-cart -->
-        </div> <!-- cd-cart-container -->
-        <?php
-    }
-
-    /**
-    * Load admin scripts
-    *
-    * @since 1.0.0
-    *
-    * @return void
-    **/
-    public function admin_enqueue_scripts( $hooks ) {
-        // Load your admin scripts..
-    }
-
-} // Woo_Quick_Cart_Checkout
+}
 
 $woo_quick_cart_checkout = Woo_Quick_Cart_Checkout::init();
-
-register_activation_hook( __FILE__, array( 'Woo_Quick_Cart_Checkout', 'activate' ) );
-register_deactivation_hook( __FILE__, array( 'Woo_Quick_Cart_Checkout', 'deactivate' ) );
